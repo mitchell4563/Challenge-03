77ad9419e3bb85d23368525de557a9e000a015ce
Mitchell Wood <mitchellwood11@gmail.com>
2022-10-01 04:25:27 -0500

    completed-project

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Challenge-03
+## <Challenge-03>
+
+## Description
+
+My motivation for building this project was to create a password generator that could be used to obtain a truly random and secure password.
+I built this project so that when I need a password I can use a generator to create a secure one.
+It solves the problem of having to use the same unsafe password for every website, if paired with a text document you could create a list of all created passwords
+Throughout this project I learned a lot about Javascript. Random number functions, list item iteration, and how frequently you use variables to just name a fraction of them.
+
+## Installation
+
--- script.js
+++ script.js
@@ -1,6 +1,96 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Function for a random integer
+function ranInt(min, max) {
+  if (!max) {
+    max = min
+    min = 0
+  }
+  var ran = Math.random()
+  return Math.floor(min*(1 - ran) + ran*max)
+}
+
+
+// Function for a random item from a generated list
+function ranItem(list) {
+  return list[ranInt(list.length)]
+}
+
+//"Brain" of application starts here
+function generatePassword() {
+
+  //Length is more expansive due to requiring a number value instead of the string returned by window.prompt
+  var userInput = window.prompt("How many characters do you want in your password?")
+  //parseInt converts a valid string input to a number data type
+  var Length = parseInt(userInput)
+  //Function to ensure user input was a valid number, quits function if not valid
+  if (isNaN(Length)) {
+    window.alert("Please enter a valid number")
+    return
+  }
+
+  //Function to ensure password length falls between 8-128 characters, quits function if not
+  if (Length < 8 || Length > 128) {
+    window.alert("Please enter a number between 8 and 128")
+    return
+  }
+
+  //Variables that return true or false based on the users password character preference 
+  var confirmNumbers = window.confirm("Do you want numbers in your password?")
+  var confirmUpper = window.confirm("Do you want uppercase letters in your password?")
+  var confirmLower = window.confirm("Do you want lowercase letters in your password?")
+  var confirmSymbols = window.confirm("Do you want symbols in your password?")
+
+  //Arrays containing each of the character types
+  var numberElements = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var upperElements = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  var lowerElements = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var symbolElements = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "-"]
+
+  //List made to contain the confirmed character type lists
+  var passwordElements = []
+
+  //Functions to "push" each character type array into the "passwordElements" list if the confirm variable returns true
+  if (confirmNumbers) {
+    passwordElements.push(numberElements)
+  }
+
+  if (confirmUpper) {
+    passwordElements.push(upperElements)
+  }
+
+  if (confirmLower) {
+    passwordElements.push(lowerElements)
+  }
+
+  if (confirmSymbols) {
+    passwordElements.push(symbolElements)
+  }
+
+
+  //Safety function for when all character types are declined but a valid length is selected, quits function if none are selected
+  if (passwordElements.length === 0) {
+    window.alert("Please Select at least one character type.")
+    return
+  }
+
+  //Variable for the final generated string
+  var finalPassword = ""
+
+  //for loop to iterate through each selected list and choose a random element out of each one of them until the entered length is exceeded
+  for (var i = 0; i < Length; i++) {
+    var ranElementList = ranItem(passwordElements)
+    var ranChar = ranItem(ranElementList)
+    finalPassword += ranChar
+  }
+  //Quits a successful function and returns the final generated password to the original writePassword function which prints it onto the screen
+  return finalPassword
+
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
